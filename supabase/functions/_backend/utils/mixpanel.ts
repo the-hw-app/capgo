5b525a14d6cc11ed8bdc491642f6bcd65fa0c95b
r-kohale9 <r.kohale9@gmail.com>
2024-12-04 14:44:14 +0530

    ref: parse event name

--- supabase/functions/_backend/utils/mixpanel.ts
+++ supabase/functions/_backend/utils/mixpanel.ts
@@ -18,17 +18,24 @@ export const sendMixpanelStudentEvent = async (
         .single()
 
     if (!deviceInfo?.custom_id) return;
-    const teacherStudentParams = {
-        distinct_id: deviceInfo?.custom_id,
-        device_id: deviceInfo.device_id
-    };
-
     const eventBody = {
-        ...teacherStudentParams,
+        distinct_id: deviceInfo?.custom_id,
+
         ...params,
-        triggered_via: "backend",
+        triggered_via: "capgo_backend",
     };
 
-    console.log("🚀 ~ eventBody:", event_name, eventBody)
-    mixpanel.track(event_name, eventBody);
+    if (params.version_id) {
+        const { data: versionInfo } = await supabaseAdmin(context)
+            .from('app_versions')
+            .select('id, name')
+            .eq('id', params.version_id)
+            .single()
+
+        eventBody['version_name'] = versionInfo?.name
+    }
+
+    const camelToSnakeCase = (str: any) => str.replace(/[A-Z]/g, (letter: any) => `_${letter.toLowerCase()}`);
+    console.log("🚀 ~ eventBody:", camelToSnakeCase(event_name), eventBody)
+    mixpanel.track(camelToSnakeCase(event_name), eventBody);
 };
